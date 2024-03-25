7b85231037c60a764f708c4081324ce3324c0c58
Wilfried Woivre <wilfried.woivre@gmail.com>
2024-03-25 07:05:51 +0100

    Update search-news.js

--- js/search-news.js
+++ js/search-news.js
@@ -15,10 +15,10 @@
             $(this).toggle(finding);
             if (finding) {
                 if (input == '') {
-                    ($(this).find("a")[0]).innerText = "Lire";
+                    ($(this).find("a")[0]).innerText = "Read";
                 } else {
                     var count = searchData.filter(e => e.url == $(this).data('url'))[0].content.match(new RegExp(input, 'g')).length;
-                    ($(this).find("a")[0]).innerText = "Lire " + count + " news";
+                    ($(this).find("a")[0]).innerText = "Read " + count + " news";
                 }
                 var param = encodeURI(input);
                 ($(this).find("a")[0]).href = "{{ site.url }}" + $(this).data('url') + "?search=" + param;
